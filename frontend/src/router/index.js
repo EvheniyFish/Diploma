e997e98fa71915a28ae5f6aa24f60d36df4fccd6
EvheniyFish <vt222_fyeo@student.ztu.edu.ua>
2026-05-11 13:01:00 +0300

    frontend: Vue 3 app bootstrap, PrimeVue, Pinia, router with 5 views

---
+++ frontend/src/router/index.js
@@ -0,0 +1,11 @@
+﻿import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  { path: "/", name: "dashboard", component: () => import("../views/DashboardView.vue") },
+  { path: "/units", name: "units", component: () => import("../views/UnitsListView.vue") },
+  { path: "/units/:id", name: "unit-detail", component: () => import("../views/UnitDetailView.vue") },
+  { path: "/models", name: "models", component: () => import("../views/ModelsListView.vue") },
+  { path: "/events", name: "events", component: () => import("../views/EventsView.vue") },
+];
+
+export default createRouter({ history: createWebHistory(), routes });
